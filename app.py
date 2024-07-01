d8ee048c74dc52a675f5e72663184708a63e66f9
TONMOY BORUAH <whiteDwarf79>
2024-07-01 22:52:17 +0530

    Update app.py with changes

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def process_segment():
 
         result = predict_audio_prominence(segment_path, model_path='model/model.pkl')
 
-        # Check if the 'prominent' property exists in the result
+        # Check if the 'prominent' property exists or not in the result
         if 'prominent' in result:
             prominent_value = result['prominent']
         else:
